268ad407c794cd458d87ac3f89412ce96e7d5020
CFSilveira <97297875+CFSilveira@users.noreply.github.com>
2022-02-17 14:42:05 +0000

    Update User.model.js

--- models/User.model.js
+++ models/User.model.js
@@ -5,7 +5,6 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      // I like working with Plipa
       // unique: true -> Ideally, should be unique, but its up to you
     },
     password: String,
